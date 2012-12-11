1b46fa757fe714068f86685937bbbe11d98665a0
Colyn Brown <colynb@auction.com>
2012-12-10 17:16:31 -0800

    initial app files

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'a01f4686e0a273600c003498548c03ab8365556d10febd32a56630f85827b01a7e1938645a1c564215278dfcb034e9f114bf5c8251f00a33ff4eefe142b02cf9'
